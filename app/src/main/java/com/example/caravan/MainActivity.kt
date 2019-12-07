a94063629b89f10f8edeca2d3cffe5ddf94c2668
yiftachnachman <nachmanyiftach@gmail.com>
2019-12-07 13:15:42 -0800

    jagoda help me

--- app/src/main/java/com/example/caravan/MainActivity.kt
+++ app/src/main/java/com/example/caravan/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.caravan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.loginin_screen.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.loginin_screen)
+
+        signupbtntosignuppage.setOnClickListener{
+            val intent = Intent(this, Sign_up_page::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/caravan/Sign_up_page.kt
@@ -0,0 +1,36 @@
+package com.example.caravan
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import com.example.caravan.R
+import kotlinx.android.synthetic.main.sign_up_screen.*
+
+class Sign_up_page : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up_screen)
+
+        //take values from the sign up screen
+        val usernamesignupfield = findViewById<EditText>(R.id.usernamesignupfield)
+        val passwordsignupfield = findViewById<EditText>(R.id.passwordsignupfield)
+        val signupbtnsignuppage = findViewById<Button>(R.id.signupbtnsignuppage)
+
+        //handle button click
+        signupbtnsignuppage.setOnClickListener {
+            //Get texts from text edittexts
+           val username = usernamesignupfield.text.toString()
+           val password = passwordsignupfield.text.toString()
+
+            //intent to start activity
+            //var intent = Intent(thisMainActivity, Sign_up_page::class.java)
+            //intent.putExtra("usernamesignupfield", username)
+            //intent.putExtra("passwordsignupupfield", password)
+            //startActivity(intent)
+        }
+    }
+}
