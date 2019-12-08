7d7befcfc23c2225966066b3e00874c5fce14f2d
jagod101 <jagod101@mail.chapman.edu>
2019-12-07 17:08:49 -0800

    Choice Screen Implementation

--- app/src/main/java/com/example/caravan/MainActivity.kt
+++ app/src/main/java/com/example/caravan/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.caravan
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.net.Uri
 import kotlinx.android.synthetic.main.loginin_screen.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +23,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, Choice_Screen::class.java)
             startActivity(intent)
         }
-        //spotifybtn.setOnClickListener{
-        //    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("")))
-       //}
+        spotifyLink.setOnClickListener{
+            //val intent = Intent.ACTION_VIEW, Uri.parse("https://accounts.spotify.com/en/login?continue=https:%2F%2Fopen.spotify.com%2Fartist%2F7mz6tB1Og2yzxP74wxDVrn\"")
+            //startActivity(intent)
+       }
+
     }
 }
