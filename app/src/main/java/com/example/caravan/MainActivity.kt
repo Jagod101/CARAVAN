394455a3fa0ee9e276b85c52ea9d706257661bda
jagod101 <jagod101@mail.chapman.edu>
2019-12-09 13:55:12 -0800

    Added YT Player

--- app/src/main/java/com/example/caravan/MainActivity.kt
+++ app/src/main/java/com/example/caravan/MainActivity.kt
@@ -4,12 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.net.Uri
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 import kotlinx.android.synthetic.main.loginin_screen.*
 
 class MainActivity : AppCompatActivity() {
 
     //check for user
     //private fun getUserName() = intent.extras?.get("username").toString().trim()
+//    companion object {
+//        val CLIENT_ID: String = "0ac0be38dfa24683a2f3150bbbe4811e"
+//        val REDIRECT_URI : String = "com.example.caravan://callback"
+//    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +36,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         spotifyLink.setOnClickListener {
-            SpotifyService.connect(this) {
-                val intent = Intent(this, PlayerActivity::class.java)
-                startActivity(intent)
-            }
+            val intent = Intent(this, PlayerActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/caravan/PlayerActivity.kt
+++ app/src/main/java/com/example/caravan/PlayerActivity.kt
@@ -2,39 +2,30 @@ package com.example.caravan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import kotlinx.android.synthetic.main.activity_player.*
 
-class PlayerActivity : AppCompatActivity() {
+
+class PlayerActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+    override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
+        Toast.makeText(applicationContext, "Youtube Api Initialization Success", Toast.LENGTH_SHORT).show()
+        if (!wasRestored) {
+            player?.cueVideo("oHg5SJYRHA0");
+        }
+    }
+
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+        Toast.makeText(applicationContext, "Youtube Api Initialization Failure", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
-        setupViews()
-        setupListeners()
-    }
-    private fun setupViews () {
-        SpotifyService.playingState {
-            when(it) {
-//                PlayingState.PLAYING -> showPauseButton()
-//                PlayingState.STOPPED -> showPlayButton()
-//                PlayingState.PAUSED -> showResumeButton()
-            }
-        }
-    }
-
-    private fun setupListeners() {
-//        playButton.setOnClickListener {
-//            SpotifyService.play("spotify:album:5L8VJO457GXReKVVfRhzyM")
-//            showPauseButton()
-//        }
-//
-//        pauseButton.setOnClickListener {
-//            SpotifyService.pause()
-//            showResumeButton()
-//        }
-//
-//        resumeButton.setOnClickListener {
-//            SpotifyService.resume()
-//            showPauseButton()
-//        }
+        ytPlayer.initialize("AIzaSyBCKToHxl8fRb1EbJtyP1UExuQL3J554S8", this)
     }
 }
